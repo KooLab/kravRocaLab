059ab083dd21880b41ab8c7ec00f5ad0d8d6a277
KrackenRoca <juanluisRoca360@gmail.com>
2019-09-10 19:04:42 -0600

    Update startconection.sh

--- startconection.sh
+++ startconection.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+#version:0.0.0.2  util en KDE5 plasma
+#mar sep 10 18:19:08 MDT 2019
+#asignas a variable
+PASS=$1
+PASSR=$2
 
+for ip_offline in NameHost.io 127.0.0.1
+do
+
+konsole --hold --new-tab -e $SHELL -c "echo $PASSR ;  sshpass -p $PASS ssh -o StrictHostKeyChecking=no sysadmin@$ip_offline" &
+done
+wait
